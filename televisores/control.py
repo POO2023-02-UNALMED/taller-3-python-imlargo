345c60df703135f0c94b489ea8c7bebc2c286f91
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2023-08-25 11:00:11 -0500

    Add files via upload

---
+++ televisores/control.py
@@ -0,0 +1,51 @@
+class Control:
+
+    def __init__(self, tv):
+      self._tv = tv
+
+
+    #------------------------------
+    def turnOff(self):
+        self._tv.turnOff()   
+    
+    def turnOn(self):
+        self._tv.turnOn()
+    
+    #......
+    def canalUp(self):
+        self._tv.canalUp()
+    
+    def canalDown(self):
+        self._tv.canalDown()
+    
+    #......
+    def volumenUp(self):
+        self._tv.volumeUp()
+    
+    def volumenDown(self):
+        self._tv.volumeDown()
+    
+    #......
+    def setCanal(self, canal):
+        self._tv.setCanal(canal)
+    
+    def setVolumen(self, volumen):
+        self._tv.setVolumen(volumen)
+    
+    #------------------------------
+    
+
+    #------------------------------
+    def enlazar(self, tv):
+        self._tv = tv
+        tv.setControl(self)
+    
+    #------------------------------
+
+    def setTv(self, tv):
+        self._tv = tv
+    
+
+    def getTv(self):
+        return self._tv
+    
---
+++ televisores/marca.py
@@ -0,0 +1,12 @@
+class Marca:
+
+    def __init__(self, nombre):
+      self._nombre = nombre
+
+
+    def getNombre(self, nombre):
+        return self._nombre
+    
+
+    def setNombre(self, nombre):
+        self._nombre = nombre
